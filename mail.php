cc121f59a688921957bebaaa66cf416d3540bf3e
peluxVlad <64081754+peluxVlad@users.noreply.github.com>
2020-04-27 11:32:02 +0300

    Add files via upload

--- mail.php
+++ mail.php
@@ -1,4 +1,8 @@
 <?php 
+
+$name = $_POST['user_name'];
+$mail = $_POST['user_mail']; 
+
 if( $_POST){
 
   require 'phpmailer.php';
@@ -11,40 +15,30 @@ $mail->isSMTP();
   $mail->Host = 'smtp.gmail.com';
   $mail->SMTPAuth = true;
   $mail->CharSet = 'UTF-8';
-  $mail->Username = 'dsadassadasdasdas@gmail.com'; // логин от вашей почты
-  $mail->Password = 'aopmaks12'; // пароль от почтового ящика
+  $mail->Username = 'dsadassadasdasdas@gmail.com'; 
+  $mail->Password = 'aopmaks12'; 
   $mail->SMTPSecure = 'ssl';
   $mail->Port = 465;
-  $mail->From = 'info@gmail.com '; // адрес почты, с которой идет отправка
-  $mail->FromName = 'Сообщение с gmail.com'; // имя отправителя
-  $mail->addAddress('info@gmail.com');
- 
- // Прикрепление файлов
-  for ($ct = 0; $ct < count($_FILES['userfile']['tmp_name']); $ct++) {
-        $uploadfile = tempnam(sys_get_temp_dir(), sha1($_FILES['userfile']['name'][$ct]));
-        $filename = $_FILES['userfile']['name'][$ct];
-        if (move_uploaded_file($_FILES['userfile']['tmp_name'][$ct], $uploadfile)) {
-            $mail->addAttachment($uploadfile, $filename);
-        } else {
-            $msg .= 'Failed to move file to ' . $uploadfile;
-        }
-    }
- 
-// Письмо
+
+
+  $mail->setFrom('info@gmail.com'); 
+  $mail->addAddress('dsadassadasdasdas@gmail.com');
+
 $mail->isHTML(true);
-$mail->Body = "Имя: {$_POST['name']}<br> Телефон: {$_POST['nomer']}<br> Email: {$_POST['email']}<br> Сообщение: " . nl2br($_POST['body']);
-$mail->AltBody = "Имя: {$_POST['name']}\r\n Телефон: {$_POST['nomer']}\r\n Email: {$_POST['email']}\r\n Сообщение: {$_POST['body']}";
-// $mail->SMTPDebug = 0;
+$mail->Subject = 'ssaddas';
+$mail->Body = '
+	Польв отпар сообщения <br>
+	Имя: ' . $name . ' <br>
+	Почта: ' . $mail . '';
+$mail->AltBody = 'dsaddas';
 
   if( $mail->send() ){
-    $answer = '1';
-  }else{
-    $answer = '0';
     echo 'Письмо не может быть отправлено. ';
     echo 'Ошибка: ' . $mail->ErrorInfo;
-  }
+ } else{
+   	echo "Отправлено"; 
+      }
   die( $answer );
-
 ?>
 
 
